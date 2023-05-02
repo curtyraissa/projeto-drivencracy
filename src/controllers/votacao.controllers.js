fdc7e793a96f8bc28afc3f263e4701dfc7aa30b7
curtyraissa <raissacurty@gmail.com>
2023-05-02 16:49:54 -0300

    elaboracao inicial das funcoes no controllers

---
+++ src/controllers/votacao.controllers.js
@@ -0,0 +1,47 @@
+export async function createEnquete(req, res){
+    try {
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function getEnquete(req, res){
+    try {
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function createOpcaoVoto(req, res){
+    try {
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function getOpcaoVoto(req, res){
+    try {
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function createVoto(req, res){
+    try {
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function getResultado(req, res){
+    try {
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
