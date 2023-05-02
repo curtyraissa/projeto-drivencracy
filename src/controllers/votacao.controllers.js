8f48c3a6b86cae35d38d9165ec12276d11f7a959
curtyraissa <raissacurty@gmail.com>
2023-05-02 19:06:17 -0300

    metodo post /poll

--- src/controllers/votacao.controllers.js
+++ src/controllers/votacao.controllers.js
@@ -1,6 +1,15 @@
-export async function createEnquete(req, res){
-    try {
+import { db } from "../database/database.config.js"
+import dayjs from "dayjs"
+import { ObjectId } from "mongodb"
 
+export async function createEnquete(req, res){
+    const {title, expireAt} = req.body
+    if(!expireAt){
+        expireAt = dayjs().add(30, 'day').format("YYYY-MM-DD HH:mm")
+    }
+    try {
+        await db.collection("enquete").insertOne({title, expireAt})
+        res.sendStatus(201)
     } catch (err) {
         res.status(500).send(err.message)
     }
