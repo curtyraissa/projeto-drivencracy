71f92c6f854566d1f257f57ce0689ac00cf68236
curtyraissa <raissacurty@gmail.com>
2023-05-03 14:32:53 -0300

    validacao com joi

--- src/controllers/votacao.controllers.js
+++ src/controllers/votacao.controllers.js
@@ -1,12 +1,22 @@
 import { db } from "../database/database.config.js"
 import dayjs from "dayjs"
 import { ObjectId } from "mongodb"
+import { opcaoDeVotoSchema, enqueteSchema } from "../schemas/schemas.js"
 
 export async function createEnquete(req, res){
     const {title, expireAt} = req.body
+
+    const validation = enqueteSchema.validate(req.body, {abortEarly: false})
+
+    if(validation.error){
+        const errors = validation.error.details.map((d) => d.message)
+        return res.status(422).send(errors)
+    }
+
     if(!expireAt){
         expireAt = dayjs().add(30, 'day').format("YYYY-MM-DD HH:mm")
     }
+
     try {
         await db.collection("enquete").insertOne({title, expireAt})
         res.sendStatus(201)
@@ -27,6 +37,16 @@ export async function getEnquete(req, res){
 export async function createOpcaoVoto(req, res){
     const {title, pollId} = req.body
 
+    const validation = opcaoDeVotoSchema.validate(req.body, {abortEarly: false})
+
+    if(validation.error){
+        const errors = validation.error.details.map((d) => d.message)
+        return res.status(422).send(errors)
+    }
+
+    const enquete = await db.collection("enquete").find({_id: new ObjectId(id)})
+    if(!enquete) return res.sendStatus(404)
+
     try {
         await db.collection("opcaoDeVoto").insertOne({title, pollId})
         res.sendStatus(201)
@@ -38,8 +58,8 @@ export async function createOpcaoVoto(req, res){
 export async function getOpcaoVoto(req, res){
     const {id} = req.params
     try {
-        const enquetes = await db.collection("opcaoDeVoto").find({_id: new ObjectId(id)}).toArray()
-        res.send(enquetes)
+        const opcaoDeVoto = await db.collection("opcaoDeVoto").find({_id: new ObjectId(id)}).toArray()
+        res.send(opcaoDeVoto)
     } catch (err) {
         res.status(500).send(err.message)
     }
@@ -48,7 +68,9 @@ export async function getOpcaoVoto(req, res){
 export async function createVoto(req, res){
     const {id} =req.params
     try {
-        await db.collection("voto").insertOne({createdAt: dayjs().format("YYYY-MM-DD HH:mm"), choiceId: id})
+        const opcaoDeVoto = await db.collection("opcaoDeVoto").find({_id: new ObjectId(id)})
+        if(!opcaoDeVoto) return res.sendStatus(404)
+        await db.collection("voto").insertOne({createdAt: dayjs().format("YYYY-MM-DD HH:mm"), choiceId: new ObjectId(id)})
         res.sendStatus(201)
     } catch (err) {
         res.status(500).send(err.message)
